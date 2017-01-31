1cd616ae2b0b6d962365965c2db41dd871cea353
Yann Dupont <yaanst@gmail.com>
2017-01-31 12:02:14 +0100

    Improved FDDBScraper to remove generic words from product names

--- scrap/fddb.py
+++ scrap/fddb.py
@@ -2,6 +2,10 @@ import scrapy,re
 from bs4 import BeautifulSoup as bs
 
 class FddbSpider(scrapy.Spider):
+    """
+        Scrapy spider meant to crawl all nutritional information 
+        about a product from http://fddb.info
+    """
     name = "fddb"
     start_urls = [
         "http://fddb.info/db/fr/groupes/{}/index.html".format(group)
@@ -12,21 +16,14 @@ class FddbSpider(scrapy.Spider):
             "friterie", "fruits", "huiles_et_lipides", "legumes", 
             "laitage", "poisson", "sucreries"]
         ]
-    custom_settings =  {
-#        "CONCURRENT_REQUESTS": 1,
-#        "CONCURRENT_ITEMS": 5,
-#        "CONCURRENT_REQUESTS_PER_DOMAIN": 1,
-        "DUPEFILTER_DEBUG": True,
-            }
 
     
     def parse(self, response):
         # Parse groups if there are any
         groups_b = response.css('div.leftblock h3:first-child::text').extract_first()
         if groups_b:
-            #groups = response.css('div.leftblock div.standardcontent:first-child table td:first-child a::attr(href)').extract()
+            # Follow the "groups" links 
             groups = response.css('div.leftblock div.standardcontent:first-child table td table td:first-child a::attr(href)').extract()
-            print("From",response.url,":\n(groups)\n",groups)
             for group in groups:
                 group_page = response.urljoin(group.strip())
                 yield scrapy.Request(group_page, callback=self.parse)
@@ -37,7 +34,6 @@ class FddbSpider(scrapy.Spider):
             nb_child = "n + 2" if groups_b else "n"
             query = 'div.leftblock > div:nth-child({}) table a::attr(href)'.format(nb_child)
             products = response.css(query).extract()
-            print("From",response.url,":\n(products)\n",products)
             for product in products:
                 product_page = response.urljoin(product.strip())
                 yield scrapy.Request(product_page, callback=self.parse_product)
@@ -46,11 +42,12 @@ class FddbSpider(scrapy.Spider):
 
     def parse_product(self, response):
         nutriments = []
-        regex = r"^([\d,]+)\s(\w+)$"
+        value_unit_re = r"^([\d,]+)\s(\w+)$"
         
         # Extracts all nutriments information
         if response.css("div.leftblock div.itemsec2012:first-child h2::text").extract_first():
-            rows = response.css('div.itemsec2012:first-child ~ div > div').extract() # selects all interesting rows
+            rows = response.css('div.itemsec2012:first-child ~ div > div').extract() 
+            # Even rows are nutriments' names, odd rows are nutriments' values and units
             i = 0
             while i < len(rows)-1:
                 nutriment = {}
@@ -58,12 +55,13 @@ class FddbSpider(scrapy.Spider):
                 html_value = bs(rows[i+1], 'html.parser')
                 i += 2
 
-                # Skip vitamins and water content
+                # Skip "Water content"
                 name = html_name.span.string
                 if "Water" in name:
                     continue
                 
-                # Use same names as those already in ElasticSearch
+                # Rename nutriments with the names 
+                # already present in our ElasticSearch (ES) instance
                 if name == "Valeur énergétique":
                     name = "Énergie"
                 elif name == "Calorie":
@@ -73,8 +71,8 @@ class FddbSpider(scrapy.Spider):
                 elif name == "Sucre":
                     name = "Sucres"
             
-                # Sperate value and unit
-                value, unit = re.match(regex, html_value.text).groups()
+                # Sperate values and units
+                value, unit = re.match(value_unit_re, html_value.text).groups()
                 value = float(value.replace(',', '.'))
 
                 if unit == "kcal":
@@ -90,11 +88,12 @@ class FddbSpider(scrapy.Spider):
                 nutriments.append(nutriment)
         
         unit = 'ml' if 'ml' in response.css("div.leftblock div.itemsec2012:first-child h2::text").extract_first() else 'g'
+        # Output ready to be sent to ES
         yield {
                 '_index': "products",
                 '_type': "FDDB",
                 '_source': {
-                    'name': response.css("h1#fddb-headline1::text").extract_first(),
+                    'name': self.clean_name(response.css("h1#fddb-headline1::text").extract_first()),
                     'unit': unit,
                     'unit_quantity': unit,
                     'unit_portion': 0,
@@ -102,3 +101,19 @@ class FddbSpider(scrapy.Spider):
                     'nutriments': nutriments
                     }
                 }
+
+    def clean_name(self, name):
+        # Removes generic words inside product name
+        reg = r",\s("\
+                "séché[es]*|" \
+                "sèches?|" \
+                "secs?|" \
+                "cuit[es]*|" \
+                "grain[es]*|" \
+                "frais|" \
+                "fra[iî]ches?|" \
+                "cru[es]*|" \
+                "en moyenne|" \
+                "moyen(ne)?" \
+                ")$"
+        return re.sub(reg, '', name)
